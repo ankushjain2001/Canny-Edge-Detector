8e5fd426105303bc0b4e6c34297536fa4eb449c6
Ankush Jain <ankushjain2001@yahoo.co.in>
2019-11-06 17:35:42 -0500

    Implemented NMS and double thresholding

--- canny_edge.py
+++ canny_edge.py
@@ -7,7 +7,7 @@ Created on Tue Oct 29 20:11:28 2019
 import numpy as np
 import matplotlib.pyplot as plt
 from math import floor, degrees, atan2
-from cv2 import imread, imshow, waitKey
+from cv2 import imread, imshow, imwrite, waitKey
 
 # Function for performing Convolution
 def convolution(m1, m2, maskSum):
@@ -19,7 +19,7 @@ def convolution(m1, m2, maskSum):
             mSum += (int(m1[i, j]) * int(m2[i, j]))
     return int(round(mSum/maskSum))
 
-#
+# Function for getting gradient magnitude and angle from X & Y gradient
 def gradient_mag_and_angle(gradX, gradY, gradientSize, gaussianSize):
     # Setting mask parameters
     outMag = np.zeros(gradX.shape, dtype='uint8')
@@ -71,7 +71,7 @@ def gaussian_smoothing(img, maskSize):
     return out
             
 # Function for Gradient Operation
-def gradient_operation(img, maskSize, axis, gaussianSize):
+def gradient_operation(img, axis, maskSize, gaussianSize):
     # Setting mask parameters
     out = np.zeros(img.shape, dtype='int8')
     # Variable to hold undefined area from gaussian filtering
@@ -105,28 +105,119 @@ def gradient_operation(img, maskSize, axis, gaussianSize):
                 out[i,j] = convolution(mask, windowPixels, 1)
     return out
 
-def non_maxima_suppression():
-    pass
+def non_maxima_suppression(gradMag, gradAng, gradientSize, gaussianSize):
+    # Setting mask parameters
+    out = np.zeros(gradMag.shape, dtype='uint8')
+    # Variable to hold undefined area from gaussian filtering
+    # To process 8-connected neighbors of NMS,1 more pixel is added to border
+    undefPixels = floor(gaussianSize/2) + floor(gradientSize/2) + 1
+    # Getting img dimensions
+    n, m = gradMag.shape
+    # Starting gaussian masking
+    for i in range(undefPixels, n-undefPixels):
+        for j in range(undefPixels, m-undefPixels):
+            # Sector 0 neighbors
+            if (337.5<=gradAng[i][j]<=360) or (0<=gradAng[i][j]<22.5) or (157.5<=gradAng[i][j]<202.5):
+                neighbor1 = gradMag[i][j+1]
+                neighbor2 = gradMag[i][j-1]
+            # Sector 1 neighbors
+            elif (22.5<=gradAng[i][j]<67.5) or (202.5<=gradAng[i][j]<247.5):
+                neighbor1 = gradMag[i-1][j+1]
+                neighbor2 = gradMag[i+1][j-1]
+            # Sector 2 neighbors
+            elif (67.5<=gradAng[i][j]<112.5) or (247.5<=gradAng[i][j]<292.5):
+                neighbor1 = gradMag[i-1][j]
+                neighbor2 = gradMag[i+1][j]
+            # Sector 3 neighbors
+            elif (112.5<=gradAng[i][j]<157.5) or (292.5<=gradAng[i][j]<337.5):
+                neighbor1 = gradMag[i-1][j-1]
+                neighbor2 = gradMag[i+1][j+1]
+            # Default case
+            else:
+                neighbor1 = 255
+                neighbor2 = 255
+            # Comparing current pixel with its neighbors
+            if gradMag[i][j] > neighbor1 and gradMag[i][j] > neighbor2:
+                out[i][j] = gradMag[i][j]
+    return out
+    
 
-def thresholding():
-    pass
-
-
-
+def thresholding(nmsImg, gradMag, gradAng, t, gradientSize, gaussianSize):
+    # Setting mask parameters
+#    nmsImg = resultNMS 
+#    gradMag = resultGradMag 
+#    gradAng = resultGradAng 
+#    t = 50
+#    gradientSize = 3
+#    gaussianSize = 7
+    out = np.zeros(nmsImg.shape, dtype='uint8')
+    # Setting threshold values
+    t1 = t
+    t2 = 2*t
+    # Variable to hold undefined area from gaussian filtering
+    # To process 8-connected neighbors of NMS,1 more pixel is added to border
+    undefPixels = floor(gaussianSize/2) + floor(gradientSize/2) + 1
+    # Getting img dimensions
+    n, m = nmsImg.shape
+    # Starting gaussian masking
+    for i in range(undefPixels, n-undefPixels):
+        for j in range(undefPixels, m-undefPixels):
+            
+            #i=8
+            #j=10
+            
+            # Case 1 with pixel less than T1
+            if nmsImg[i][j] < t1:
+                out[i][j] = 0
+            # Case 2 with pixel between T1 and T2
+            elif t1 <= nmsImg[i][j] <= t2:
+                # Extracting 8-neighbor magnitudes
+                neighborMags = [gradMag[i-1,j-1], gradMag[i-1,j], gradMag[i-1,j+1],
+                                gradMag[i,j-1], gradMag[i,j+1],
+                                gradMag[i+1,j-1], gradMag[i+1,j], gradMag[i+1,j+1]]
+                # Extracting 8-neighbor angles
+                neighborAngs = [gradAng[i-1,j-1], gradAng[i-1,j], gradAng[i-1,j+1],
+                                gradAng[i,j-1], gradAng[i,j+1],
+                                gradAng[i+1,j-1], gradAng[i+1,j], gradAng[i+1,j+1]]
+                # Comparing the 8-neighbors of the current pixel with T2
+                val = 0
+                for k in range(8):
+                    #print(neighborMags[k], abs(neighborAngs[k] - gradAng[i][j]))
+                    if neighborMags[k] > t2 and abs(neighborAngs[k] - gradAng[i][j]) <= 45:
+                        val = 255
+                        #print(val)
+                        break
+                out[i][j] = val
+            # Case 3 with pixel greater than T2
+            elif nmsImg[i][j] > t2:
+                out[i][j] = 255
+    return out
 # MAIN
 
-#path = 'data/Houses-225.bmp'
-path = 'data/Zebra-crossing-1.bmp'
+path = 'data/chess.bmp'
+#path = 'data/Zebra-crossing-1.bmp'
 img = imread(path, 0) 
 resultGauss = gaussian_smoothing(img, 7)
-resultGradX = gradient_operation(resultGauss, 3, 'x', 7)
-resultGradY = gradient_operation(resultGauss, 3, 'y', 7)
+resultGradX = gradient_operation(resultGauss, 'x', 3, 7)
+resultGradY = gradient_operation(resultGauss, 'y', 3, 7)
 resultGradMag, resultGradAng = gradient_mag_and_angle(resultGradX, resultGradY, 3, 7)
+resultNMS = non_maxima_suppression(resultGradMag, resultGradAng, 3, 7)
+resultThresholding = thresholding(resultNMS, resultGradMag, resultGradAng, 25, 3, 7)
 
 imshow('image', img)
 imshow('image', resultGauss)
 imshow('image', resultGradX)
 imshow('image', resultGradY)
 imshow('image', resultGradMag)
+imshow('image', resultNMS)
+imshow('image', resultThresholding)
 
 waitKey(0)
+
+imwrite('result2.bmp', resultThresholding)
+
+# 1. Threshold t1 and t2 for the images to be inserted into the report.
+# 2. In thresholding, for case-2, we will use the 8 connected neighbors of gradMag or nmsMag matrices?
+# 3. Total number of undefined pixels on edges? 3 (gauss)+1(sobel)+1(nms)=5?
+# 4. Should the nmsMag be normalized to 0 or 255?
+
